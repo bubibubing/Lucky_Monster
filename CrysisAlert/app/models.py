762c806ac111118e4ae228733294cf29f0403120
CL <caoliu98@yahoo.com>
2018-10-02 16:35:59 +0800

    database draft

--- CrysisAlert/app/models.py
+++ CrysisAlert/app/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Crisis(models.Model):
+    
+    crisis_type = models.CharField(max_length=50)
+    
+
 class CrisisReport(models.Model):
     '''
     Crisis reports received by call center operators
@@ -12,19 +17,15 @@ class CrisisReport(models.Model):
         ('S', 'Solved')
     )
 
-    ASSISTANCE_TYPE_CHOICE = (
-        ('E', 'Emergency Ambulance'),
-        ('R', 'Rescue and Evacuation'),
-        ('F', 'Fire-Fighting'),
-        ('G', "Gas Leak Control")
-    )
-
-    street_name = models.CharField(max_length=150)
     witness_first_name = models.CharField(max_length=20)
     witness_last_name = models.CharField(max_length=20)
     mobile_number = models.CharField(max_length=8)
+
+    street_name = models.CharField(max_length=150)
+    crisis_type = models.ForeignKey('Crisis', on_delete=models.CASCADE)
     description = models.TextField()
-    type_of_assistance = models.CharField(max_length=1, choices=ASSISTANCE_TYPE_CHOICE)
+    injured_people_num = models.IntegerField()
+
     status = models.CharField(max_length=1, choices=STATUS_CHOICES)
     create_date_time = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
@@ -32,6 +33,30 @@ class CrisisReport(models.Model):
     def get_witness_name(self):
         return "{} {}".format(self.witness_first_name, self.witness_last_name)
 
-class CrisisReportDetail():
-    pass
+class Assistance(models.Model):
+    ASSISTANCE_TYPE_CHOICE = (
+        ('E', 'Emergency Ambulance'),
+        ('R', 'Rescue and Evacuation'),
+        ('F', 'Fire-Fighting'),
+        ('G', "Gas Leak Control")
+    )
+
+    crisis = models.ForeignKey('Crisis', on_delete=models.CASCADE)
+    type_of_assistance = models.CharField(max_length=1, choices=ASSISTANCE_TYPE_CHOICE)
+    agencies = models.ManyToManyField('Agencies', through='InfoDispatch')
+
+
+class InfoDispatch(models.Model):
+    assistance_type = models.ForeignKey(Assistance, on_delete=models.CASCADE)
+    agencies = models.ForeignKey('Agency', on_delete=models.CASCADE)
+    notes = models.CharField(max_length=50)
+    
+
+class Agency(models.Model):
+    name = models.CharField(max_length=50)
+    contact_num = models.CharField(max_length=6)
+
+
+class Facilities(Agency):
+    location = models.CharField(max_length=50)
 
