c7052130d1007b3153c88dd5ae576adead186f1a
Clark <caoliu98@yahoo.com>
2018-11-01 03:18:34 +0000

    server up

--- CrysisAlert/CrysisAlert/settings.py
+++ CrysisAlert/CrysisAlert/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'y_vw24s3apc#qcs=3umd3626j_fcryobzmp&^j^b%eb9f*w7kr'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["172.21.148.165",]
 
 
 # Application definition
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -56,13 +57,11 @@ MIDDLEWARE = [
 ]
 
 MIDDLEWARE_CLASSES = (
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.BrokenLinkEmailsMiddleware',
     'django.middleware.common.CommonMiddleware',
     #...
 )
 
-CORS_ORIGIN_ALLOW_ALL = True
 
 
 ROOT_URLCONF = 'CrysisAlert.urls'
@@ -84,6 +83,9 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'CrysisAlert.wsgi.application'
+
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 
 # Database
